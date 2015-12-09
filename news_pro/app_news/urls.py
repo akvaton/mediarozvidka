188620ab06dead2b65cc2086f263038d8303ff34
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2015-12-09 13:22:04 +0200

    Minor bug fixes

--- news_pro/app_news/urls.py
+++ news_pro/app_news/urls.py
@@ -6,7 +6,6 @@ from .views import (Index, get_articles, get_shares, get_time,
                     AllNews, OneNews, save_to_excel)
 
 urlpatterns = [
-    url(r'^$', Index.as_view(), name='index'),
     url(r'^all$', AllNews.as_view(), name='all'),
     url(r'^all/(?P<source>\d+)$', AllNews.as_view(), name='news_type'),
     url(r'^news/(?P<pk>\d+)$', OneNews.as_view(), name='news'),
@@ -15,6 +14,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [
+            url(r'^$', Index.as_view(), name='index'),
             url(r'^get_new$', get_articles, name='get_new'),
             url(r'^get_shares$', get_shares, name='get_shares'),
                         ]
