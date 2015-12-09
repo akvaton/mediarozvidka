48911b95a4782f9ae87f5784e004ec2bd023dafe
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2015-12-09 12:15:57 +0200

    Minor bug fixes

--- news_pro/news_pro/settings.py
+++ news_pro/news_pro/settings.py
@@ -104,7 +104,7 @@ TEMPLATE_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATICFILES_DIRS = (STATIC_ROOT,
     os.path.join(BASE_DIR, "static"),
 )
 
