30c45721cb14f6192a570b33e94982f4a423d0ec
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-05-12 13:13:30 +0300

    Minor bug fixes

--- news_pro/news_pro/settings.py
+++ news_pro/news_pro/settings.py
@@ -97,11 +97,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-try:
-    from local_settings import *
-except ImportError:
-    print ('Local settings import error')
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
@@ -135,3 +130,9 @@ LOGGING = {
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/all'
+
+try:
+    from local_settings import *
+except ImportError:
+    print ('Local settings import error')
+    
