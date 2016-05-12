a6fabb4a069f48505e5fb1f49b88509919cc3ab1
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-05-12 15:13:59 +0300

    Remove depricated views from import

--- news_pro/app_news/urls.py
+++ news_pro/app_news/urls.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django.conf.urls import url
 from django.conf import settings
-from .views import (Index, get_articles, get_shares, check,
+from .views import (Index, get_articles, get_shares,
                     AllNews, OneNews, save_to_excel)
 
 urlpatterns = [
@@ -10,7 +10,6 @@ urlpatterns = [
     url(r'^all/(?P<source>\d+)$', AllNews.as_view(), name='news_type'),
     url(r'^news/(?P<pk>\d+)$', OneNews.as_view(), name='news'),
     url(r'^xls$', save_to_excel, name='xls'),
-    url(r'^check$', check, name='check')
 ]
 
 if settings.DEBUG:
