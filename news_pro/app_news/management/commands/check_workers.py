6b248b2c29a8d6f03ff1893f4a5ae1396e438076
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-05-12 13:35:09 +0300

    Add logging to check_workers manage command

--- news_pro/app_news/management/commands/check_workers.py
+++ news_pro/app_news/management/commands/check_workers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta, datetime
+import logging`
 
 from django.core.management.base import NoArgsCommand
 from django.core.mail import EmailMultiAlternatives
@@ -7,18 +8,21 @@ from django.conf import settings
 
 from app_news.models import ArticleModel, StatisticArticle
 
+logger = logging.getLogger('error')
 
 class Command(NoArgsCommand):
     def handle(self, **options):
         now_minus_hour = datetime.today() - timedelta(hours=1)
-        last_statics_time = StatisticArticle.objects.filter(datetime__gte=now_minus_hour).exists()
-        if not last_statics_time:
+        last_statistics_time = StatisticArticle.objects.filter(datetime__gte=now_minus_hour).exists()
+        if not last_statistics_time:
             recipient_list = ['ilya.batozskiy@raccoongang.com',
                               'employer.email']
             subject, from_email = 'Some problems on News Server', 'no-reply@news.com'
             last_working_time = StatisticArticle.objects.all().order_by('datetime').\
                                                  last().datetime.replace(tzinfo=None)
             not_working = (datetime.today().replace(tzinfo=None) -  last_working_time).total_seconds() / 3600.0
+            logger.error('Check time %, last working time %s' % 
+                         (str(datetime.today()), str(last_working_time)))
             text_content = 'Щось зламалося на сервері, оновлень нема вже більше %i годин(и)' % not_working
             msg = EmailMultiAlternatives(subject, text_content, from_email, recipient_list)
             msg.send()
